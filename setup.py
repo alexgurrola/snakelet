42a3d3689ebab092abe955f7cae0913d31d6f715
alexgurrola <alexgurrola1@gmail.com>
2019-08-04 22:54:45 -0700

    Add Python 3.7 Compatibility
    Signed-off-by: alexgurrola <alexgurrola1@gmail.com>

--- setup.py
+++ setup.py
@@ -58,7 +58,8 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
 
     # What does your project relate to?
@@ -76,7 +77,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['plac', 'pymongo'],
+    install_requires=['pymongo'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
